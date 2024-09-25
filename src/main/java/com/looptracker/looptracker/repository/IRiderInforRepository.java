dc4c721d444c919f4bc15eb45bd6662ea998e2c1
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-25 16:22:18 +0700

    [update]:add existsByCitizenIdNumber,existsByLicenseNumber func

--- src/main/java/com/looptracker/looptracker/repository/IRiderInforRepository.java
+++ src/main/java/com/looptracker/looptracker/repository/IRiderInforRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRiderInforRepository extends JpaRepository<RiderInfor, Integer> {
+    boolean existsByCitizenIdNumber(String citizenIdNumber);
+    boolean existsByLicenseNumber(String licenseNumber);
 }
