b54c2f856479ceeb1fd5647b5d2efdb1e175b39a
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-25 16:22:08 +0700

    [update]:add registrationRequest func

--- src/main/java/com/looptracker/looptracker/service/IRegistrationRequestService.java
+++ src/main/java/com/looptracker/looptracker/service/IRegistrationRequestService.java
@@ -1,4 +1,6 @@
 package com.looptracker.looptracker.service;
 
+import com.looptracker.looptracker.dto.request.RegistrationRequestDto;
 public interface IRegistrationRequestService {
+    void registrationRequest(RegistrationRequestDto registrationRequestDto);
 }
