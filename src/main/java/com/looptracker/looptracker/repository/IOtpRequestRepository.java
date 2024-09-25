0ed9be544c36c2795c72f735dbb64eeda099add7
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-25 09:03:24 +0700

    [init]:IOtpRequestRepository

---
+++ src/main/java/com/looptracker/looptracker/repository/IOtpRequestRepository.java
@@ -0,0 +1,9 @@
+package com.looptracker.looptracker.repository;
+
+import com.looptracker.looptracker.entity.OtpRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IOtpRequestRepository extends JpaRepository<OtpRequest, Integer> {
+}
