3594b1afc497dfa73b12d745d8e625129bba0cd2
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-25 16:21:27 +0700

    [update]:add findByEmail func

--- src/main/java/com/looptracker/looptracker/repository/IOtpRequestRepository.java
+++ src/main/java/com/looptracker/looptracker/repository/IOtpRequestRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOtpRequestRepository extends JpaRepository<OtpRequest, Integer> {
+    OtpRequest findByEmail(String email);
 }
