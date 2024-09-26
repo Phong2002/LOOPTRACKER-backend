7cf5f53719ea97a5f59d9e11ccc0f4f0508c38ef
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:31:33 +0700

    [update]:add verifyOtp func

--- src/main/java/com/looptracker/looptracker/controller/OtpRequestController.java
+++ src/main/java/com/looptracker/looptracker/controller/OtpRequestController.java
@@ -1,4 +1,26 @@
 package com.looptracker.looptracker.controller;
 
+
+import com.looptracker.looptracker.dto.request.VerifyOtpRequest;
+import com.looptracker.looptracker.entity.OtpRequest;
+import com.looptracker.looptracker.service.OtpRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/otp")
 public class OtpRequestController {
+    @Autowired
+    private OtpRequestService otpRequestService;
+
+    @PutMapping("/verify")
+    public ResponseEntity<?> verifyOtp(@RequestBody VerifyOtpRequest verifyOtpRequest) {
+       return ResponseEntity.ok(otpRequestService.verifyOtp(verifyOtpRequest.getEmail(), verifyOtpRequest.getOtp()));
+    }
+
+
 }
