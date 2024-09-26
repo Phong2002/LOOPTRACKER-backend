82fe8c50c683b17385f9cfe4bc0b413d047010e3
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:32:49 +0700

    [update]:add confirm api

--- src/main/java/com/looptracker/looptracker/controller/RegistrationRequestController.java
+++ src/main/java/com/looptracker/looptracker/controller/RegistrationRequestController.java
@@ -2,29 +2,37 @@ package com.looptracker.looptracker.controller;
 
 import com.looptracker.looptracker.dto.request.RegistrationRequestDto;
 import com.looptracker.looptracker.dto.request.SigninForm;
-import com.looptracker.looptracker.service.RegistrationRequestService;
+import com.looptracker.looptracker.repository.IRegistrationRequestRepository;
+import com.looptracker.looptracker.service.IRegistrationRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/registration-request")
+@RequestMapping("api/v1/registration-request")
 public class RegistrationRequestController {
 
     @Autowired
-    private RegistrationRequestService registrationRequestService;
+    private IRegistrationRequestService registrationRequestService;
 
-    @PostMapping("/easy-rider")
+    @Autowired
+    private IRegistrationRequestRepository registrationRequestRepository;
+
+    @PostMapping("easy-rider")
     public ResponseEntity<?> registrationRequest(@RequestBody RegistrationRequestDto registrationRequestDto) {
         registrationRequestService.registrationRequest(registrationRequestDto);
         return ResponseEntity.ok("Gửi yêu cầu thành công");
     }
 
-    @PostMapping("/create")
+    @PostMapping("create")
     public ResponseEntity<?> create(@RequestBody SigninForm login) {
         return null;
+    }
+
+    @PostMapping("confirm")
+    public ResponseEntity<?> confirm(@RequestBody String requestId) {
+        registrationRequestService.confirm(requestId);
+        return ResponseEntity.ok("ok");
+
     }
 }
