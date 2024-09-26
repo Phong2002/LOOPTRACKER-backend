301a27fe9e8fa6794a2b80e4ce182947b94e9e68
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 15:47:05 +0700

    [update] : add gender enum

--- src/main/java/com/looptracker/looptracker/entity/Passenger.java
+++ src/main/java/com/looptracker/looptracker/entity/Passenger.java
@@ -1,5 +1,6 @@
 package com.looptracker.looptracker.entity;
 
+import com.looptracker.looptracker.entity.enums.Gender;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,10 @@ public class Passenger {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
     @Column(name = "phone_number")
     private String phoneNumber;
 
@@ -28,5 +33,4 @@ public class Passenger {
 
     @Column(name = "notes")
     private String notes;
-
 }
