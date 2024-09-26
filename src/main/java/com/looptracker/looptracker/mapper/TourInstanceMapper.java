0e75b96221a4da7154bb71bdd5a0bc44997e4456
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 16:59:02 +0700

    [init] : TourInstanceMapper

---
+++ src/main/java/com/looptracker/looptracker/mapper/TourInstanceMapper.java
@@ -0,0 +1,7 @@
+package com.looptracker.looptracker.mapper;
+
+import com.looptracker.looptracker.dto.TourInstanceDto;
+import com.looptracker.looptracker.entity.TourInstance;
+
+public interface TourInstanceMapper extends BaseMapper<TourInstance, TourInstanceDto> {
+}
