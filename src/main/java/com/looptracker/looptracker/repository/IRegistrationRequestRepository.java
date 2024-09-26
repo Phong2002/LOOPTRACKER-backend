10635d675a6c8f2e19d08856ceb2b03ed72f7450
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:30:03 +0700

    [update]:add findByEmail func

--- src/main/java/com/looptracker/looptracker/repository/IRegistrationRequestRepository.java
+++ src/main/java/com/looptracker/looptracker/repository/IRegistrationRequestRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IRegistrationRequestRepository extends JpaRepository<RegistrationRequest, Integer> {
+public interface IRegistrationRequestRepository extends JpaRepository<RegistrationRequest, String> {
     boolean existsByCitizenIdNumber(String citizenIdNumber);
     boolean existsByLicenseNumber(String licenseNumber);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+    RegistrationRequest findByEmail(String email);
 
 }
