49ca46ff66526fd072e787591c085d0fd9cba27e
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:29:50 +0700

    [update]:verifyOtp func

--- src/main/java/com/looptracker/looptracker/service/IOtpRequestService.java
+++ src/main/java/com/looptracker/looptracker/service/IOtpRequestService.java
@@ -1,4 +1,8 @@
 package com.looptracker.looptracker.service;
 
+import com.looptracker.looptracker.dto.response.VerifyOtpResponse;
+
 public interface IOtpRequestService {
+
+    VerifyOtpResponse verifyOtp(String email, String otp);
 }
