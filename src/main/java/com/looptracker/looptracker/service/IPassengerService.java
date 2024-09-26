45399fbcbfe1d8705f2b9facc43ee25f928cde28
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 15:45:35 +0700

    [update]: add     void addPassenger(PassengerDto passengerDto);                   void updatePassenger(PassengerDto passengerDto);                   void deletePassenger(List<String> listPassengerId);                   Page<PassengerDto> getPassengers(Pageable pageable); func

--- src/main/java/com/looptracker/looptracker/service/IPassengerService.java
+++ src/main/java/com/looptracker/looptracker/service/IPassengerService.java
@@ -1,4 +1,14 @@
 package com.looptracker.looptracker.service;
 
+import com.looptracker.looptracker.dto.PassengerDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface IPassengerService {
+    void addPassenger(PassengerDto passengerDto);
+    void updatePassenger(PassengerDto passengerDto);
+    void deletePassenger(List<String> listPassengerId);
+    Page<PassengerDto> getPassengers(Pageable pageable);
 }
