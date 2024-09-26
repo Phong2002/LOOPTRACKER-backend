5f71df836ca63dd919226a11f09c3d47b426571e
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 15:51:21 +0700

    [update] : throw new ex when can't found packagetour,add getAllTourPackages,getTourPackage,updateTourPackage,deleteTourPackage service func

--- src/main/java/com/looptracker/looptracker/service/TourPackageService.java
+++ src/main/java/com/looptracker/looptracker/service/TourPackageService.java
@@ -1,16 +1,19 @@
 package com.looptracker.looptracker.service;
 
+import com.looptracker.looptracker.dto.TourPackageDto;
 import com.looptracker.looptracker.dto.request.TourPackageRequest;
 import com.looptracker.looptracker.entity.DetailedItinerary;
 import com.looptracker.looptracker.entity.TourPackage;
+import com.looptracker.looptracker.exception.CustomException;
 import com.looptracker.looptracker.mapper.DetailedItineraryMapper;
 import com.looptracker.looptracker.mapper.TourPackageMapper;
 import com.looptracker.looptracker.repository.IDetailedItineraryRepository;
 import com.looptracker.looptracker.repository.ITourPackageRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -28,6 +31,10 @@ public class TourPackageService implements ITourPackageService {
     @Override
     @Transactional
     public void createTourPackage(TourPackageRequest tourPackageRequest) {
+        if(tourPackageRepository.existsById(tourPackageRequest.getTourPackage().getId())){
+            throw new CustomException(400, "TourPackage already exists");
+        }
+
         LocalDateTime now = LocalDateTime.now();
         TourPackage tourPackage = tourPackageMapper.toEntity(tourPackageRequest.getTourPackage());
         tourPackage.setCreateAt(now);
@@ -41,5 +48,33 @@ public class TourPackageService implements ITourPackageService {
             detailedItinerary.setTourPackage(tourPackage);
         }
         detailedItineraryRepository.saveAll(detailedItineraryList);
+    }
+
+    @Override
+    public Page<TourPackageDto> getAllTourPackages(Pageable pageable) {
+        Page<TourPackage> tourPackages = tourPackageRepository.findAll(pageable);
+        Page<TourPackageDto> tourPackageDtos = tourPackageMapper.toPageDto(tourPackages);
+        return tourPackageDtos;
+    }
+
+    @Override
+    public TourPackageDto getTourPackage(String id) {
+        TourPackage tourPackage = tourPackageRepository.findById(id).orElseThrow(
+                ()->  new CustomException(404, "tourPackage Not Found")
+        );
+        TourPackageDto tourPackageDto = tourPackageMapper.toDto(tourPackage);
+        return tourPackageDto;
+    }
+
+    @Override
+    @Transactional
+    public void updateTourPackage(TourPackageDto tourPackageDto) {
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteTourPackage(String id) {
+        tourPackageRepository.deleteById(id);
     }
 }
