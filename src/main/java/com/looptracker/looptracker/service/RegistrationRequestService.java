75ae86c54168408843ce28b96958051a4946c312
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:33:56 +0700

    [update]:handle after confirm registration, create new account with role easy rider

--- src/main/java/com/looptracker/looptracker/service/RegistrationRequestService.java
+++ src/main/java/com/looptracker/looptracker/service/RegistrationRequestService.java
@@ -3,16 +3,22 @@ package com.looptracker.looptracker.service;
 import com.looptracker.looptracker.dto.request.RegistrationRequestDto;
 import com.looptracker.looptracker.entity.OtpRequest;
 import com.looptracker.looptracker.entity.RegistrationRequest;
+import com.looptracker.looptracker.entity.RiderInfor;
+import com.looptracker.looptracker.entity.User;
 import com.looptracker.looptracker.entity.enums.RegistrationStatus;
+import com.looptracker.looptracker.entity.enums.Role;
+import com.looptracker.looptracker.event_listener.event.SendAccountInforEvent;
 import com.looptracker.looptracker.event_listener.event.SendOtpEvent;
-import com.looptracker.looptracker.event_listener.listener.SendOtpListener;
 import com.looptracker.looptracker.exception.CustomException;
 import com.looptracker.looptracker.mapper.RegistrationRequestMapper;
 import com.looptracker.looptracker.repository.IOtpRequestRepository;
 import com.looptracker.looptracker.repository.IRegistrationRequestRepository;
 import com.looptracker.looptracker.repository.IRiderInforRepository;
 import com.looptracker.looptracker.repository.IUserRepository;
+import com.looptracker.looptracker.security.Authentication;
 import com.looptracker.looptracker.utils.OTPGenerator;
+import com.looptracker.looptracker.utils.PasswordGenerator;
+import com.looptracker.looptracker.utils.StringUtils;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
@@ -36,6 +42,8 @@ public class RegistrationRequestService implements IRegistrationRequestService {
    private ApplicationEventPublisher applicationEventPublisher;
    @Autowired
    private IOtpRequestRepository otpRequestRepository;
+   @Autowired
+   private Authentication authentication;
     @Override
     @Transactional
     public void registrationRequest(RegistrationRequestDto registrationRequestDto) {
@@ -69,7 +77,6 @@ public class RegistrationRequestService implements IRegistrationRequestService {
         otpRequest.setOtp(otp);
         otpRequest.setCreateAt(now);
         otpRequest.setExpiresAt(now.plusMinutes(30));
-        otpRequest.setIsUsed(false);
         otpRequestRepository.save(otpRequest);
 
         Map<String, Object> model = new HashMap<>();
@@ -77,5 +84,61 @@ public class RegistrationRequestService implements IRegistrationRequestService {
         model.put("otp", otp);
         applicationEventPublisher.publishEvent(new SendOtpEvent(this,registrationRequest.getEmail(),model));
 
+    }
+
+    @Override
+    public void confirm(String requestId) {
+         RegistrationRequest registrationRequest = registrationRequestRepository.findById(requestId).orElseThrow(
+                ()-> new CustomException(400,"Không tìm thấy yêu cầu")
+        );
+        String cleanFirstName = StringUtils.removeAccent(registrationRequest.getFirstName()).toLowerCase();
+        String cleanLastName = StringUtils.removeAccent(registrationRequest.getLastName()).toLowerCase();
+        String username = generateUniqueUsername(generateUsername(cleanFirstName, cleanLastName));
+        String password = PasswordGenerator.generatePassword(10);
+        String passwordEncode = authentication.passwordEncoder().encode(password);
+
+        User user = new User();
+        user.setFirstName(registrationRequest.getFirstName());
+        user.setLastName(registrationRequest.getLastName());
+        user.setUsername(username);
+        user.setPassword(passwordEncode);
+        user.setEmail(registrationRequest.getEmail());
+        user.setPhoneNumber(registrationRequest.getPhoneNumber());
+        user.setRole(Role.EASY_RIDER);
+        user.setGender(registrationRequest.getGender());
+        String userId = userRepository.saveAndFlush(user).getId();
+        user.setId(userId);
+
+        RiderInfor riderInfor = new RiderInfor();
+        riderInfor.setUser(user);
+        riderInfor.setCitizenIdNumber(registrationRequest.getCitizenIdNumber());
+        riderInfor.setLicenseNumber(registrationRequest.getLicenseNumber());
+        riderInforRepository.save(riderInfor);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("fullName",String.join(" ",registrationRequest.getFirstName(), registrationRequest.getLastName()));
+        model.put("username", username);
+        model.put("password", password);
+        applicationEventPublisher.publishEvent(new SendAccountInforEvent(this,user.getEmail(),model));
+
+
+    }
+
+    private String generateUsername(String firstName, String lastName) {
+        String[] lastNameParts = lastName.split(" ");
+        StringBuilder initials = new StringBuilder();
+        for (String part : lastNameParts) {
+            initials.append(part.charAt(0));
+        }
+        return firstName + initials.toString();
+    }
+    private String generateUniqueUsername(String baseUsername) {
+        int count = 1;
+        String newUsername = baseUsername;
+        while (userRepository.existsByUsername(newUsername)) {
+            newUsername = baseUsername + count;
+            count++;
+        }
+        return newUsername;
     }
 }
