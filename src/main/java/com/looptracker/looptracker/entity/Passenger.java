1aa1638ab1cf6607e30041e4a0d2ddc0b7cc990b
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 08:58:41 +0700

    [init]: Passenger

---
+++ src/main/java/com/looptracker/looptracker/entity/Passenger.java
@@ -0,0 +1,32 @@
+package com.looptracker.looptracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "passengers", schema = "looptracker")
+public class Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "notes")
+    private String notes;
+
+}
