3c74a27efaf1de4e03ca2dd4bb9e81a76cf2170a
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-26 15:50:21 +0700

    [update] : add toDto ,toEntity mapping

--- src/main/java/com/looptracker/looptracker/mapper/TourPackageMapper.java
+++ src/main/java/com/looptracker/looptracker/mapper/TourPackageMapper.java
@@ -3,7 +3,13 @@ package com.looptracker.looptracker.mapper;
 import com.looptracker.looptracker.dto.TourPackageDto;
 import com.looptracker.looptracker.entity.TourPackage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TourPackageMapper extends BaseMapper<TourPackage, TourPackageDto>{
+    @Mapping(source = "detailedItineraries", target = "detailedItineraryDtoList")
+    TourPackageDto toDto(TourPackage tourPackage);
+
+    @Mapping(source = "detailedItineraryDtoList", target = "detailedItineraries")
+    TourPackage toEntity(TourPackageDto tourPackageDto);
 }
