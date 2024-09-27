1545b95e0d749e602c676c8ba045e6e73f36073b
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-27 10:18:21 +0700

    [init] : add TourInstanceController

---
+++ src/main/java/com/looptracker/looptracker/controller/TourInstanceController.java
@@ -0,0 +1,39 @@
+package com.looptracker.looptracker.controller;
+
+import com.looptracker.looptracker.dto.request.TourInstanceRequest;
+import com.looptracker.looptracker.service.TourInstanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/tour-instance")
+public class TourInstanceController {
+    @Autowired
+    private TourInstanceService tourInstanceService;
+
+    @GetMapping("get-all")
+    public ResponseEntity<?> getAllTourInstances(Pageable pageable) {
+
+        return ResponseEntity.ok(tourInstanceService.getAllTourInstances(pageable));
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<?> createTourInstance(@RequestBody TourInstanceRequest tourInstanceRequest) {
+        tourInstanceService.createTourInstance(tourInstanceRequest);
+        return ResponseEntity.ok("Successfully created tour instance");
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<?> updateTourInstance(@RequestBody TourInstanceRequest tourInstanceRequest) {
+        tourInstanceService.updateTourInstance(tourInstanceRequest);
+        return ResponseEntity.ok("Successfully updated tour instance");
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<?> deleteTourInstance(@RequestParam Long tourInstanceId) {
+        tourInstanceService.deleteTourInstance(tourInstanceId);
+        return ResponseEntity.ok("Successfully deleted tour instance");
+    }
+}
