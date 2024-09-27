2638949fc88be292e6bad4fb75ba97efad306dae
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-27 10:20:08 +0700

    [init] : change param get all to Pageable

--- src/main/java/com/looptracker/looptracker/service/ITourInstanceService.java
+++ src/main/java/com/looptracker/looptracker/service/ITourInstanceService.java
@@ -4,11 +4,12 @@ import com.looptracker.looptracker.dto.TourInstanceDto;
 import com.looptracker.looptracker.dto.request.TourInstanceRequest;
 import com.looptracker.looptracker.entity.TourInstance;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ITourInstanceService {
     void createTourInstance(TourInstanceRequest tourInstanceRequest);
     void updateTourInstance(TourInstanceRequest tourInstanceRequest);
-    void deleteTourInstance(String tourInstanceId);
-    Page<TourInstanceDto> getAllTourInstances(TourInstanceRequest tourInstanceRequest);
+    void deleteTourInstance(Long tourInstanceId);
+    Page<TourInstanceDto> getAllTourInstances(Pageable pageable);
 
 }
