eddec00e1bcb6261f29573392dc9512bea376812
Lù Minh Phong <PhongLM@vissoft.vn>
2024-09-27 10:22:57 +0700

    [fix] : add @Mapper(componentModel = "spring")

--- src/main/java/com/looptracker/looptracker/mapper/TourInstanceMapper.java
+++ src/main/java/com/looptracker/looptracker/mapper/TourInstanceMapper.java
@@ -2,6 +2,8 @@ package com.looptracker.looptracker.mapper;
 
 import com.looptracker.looptracker.dto.TourInstanceDto;
 import com.looptracker.looptracker.entity.TourInstance;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface TourInstanceMapper extends BaseMapper<TourInstance, TourInstanceDto> {
 }
