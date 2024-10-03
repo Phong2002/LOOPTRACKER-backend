078a9b5369379a4dad6ce09f568b4d91e625d8ec
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 17:00:40 +0700

    [update]: use Interface

--- src/main/java/com/looptracker/looptracker/controller/OtpRequestController.java
+++ src/main/java/com/looptracker/looptracker/controller/OtpRequestController.java
@@ -3,6 +3,7 @@ package com.looptracker.looptracker.controller;
 
 import com.looptracker.looptracker.dto.request.VerifyOtpRequest;
 import com.looptracker.looptracker.entity.OtpRequest;
+import com.looptracker.looptracker.service.IOtpRequestService;
 import com.looptracker.looptracker.service.OtpRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/otp")
 public class OtpRequestController {
     @Autowired
-    private OtpRequestService otpRequestService;
+    private IOtpRequestService otpRequestService;
 
     @PutMapping("/verify")
     public ResponseEntity<?> verifyOtp(@RequestBody VerifyOtpRequest verifyOtpRequest) {
