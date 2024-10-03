0b8309a021af38be06984dc83a0ff0eda1e1accc
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 17:06:52 +0700

    [update]: add createTourAssignment,updateTourAssignment,getAllByTourInstanceId api

--- src/main/java/com/looptracker/looptracker/controller/TourAssignmentController.java
+++ src/main/java/com/looptracker/looptracker/controller/TourAssignmentController.java
@@ -1,4 +1,35 @@
 package com.looptracker.looptracker.controller;
 
+import com.looptracker.looptracker.dto.request.TourAssignmentRequest;
+import com.looptracker.looptracker.service.ITourAssignmentService;
+import com.looptracker.looptracker.service.TourInstanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/tour-assignment")
 public class TourAssignmentController {
+    @Autowired
+    private ITourAssignmentService tourAssignmentService;
+    @Autowired
+    private TourInstanceService tourInstanceService;
+
+    @PostMapping("create")
+    public ResponseEntity<?> createTourAssignment(@RequestBody TourAssignmentRequest tourAssignmentRequest){
+        tourAssignmentService.addTourAssignment(tourAssignmentRequest);
+        return ResponseEntity.ok("Create tour assignment successfully");
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<?> updateTourAssignment(@RequestBody TourAssignmentRequest tourAssignmentRequest){
+        tourAssignmentService.updateTourAssignment(tourAssignmentRequest);
+        return ResponseEntity.ok("Update tour assignment successfully");
+    }
+
+    @GetMapping("get-all")
+    public ResponseEntity<?> getAllByTourInstanceId(@RequestParam(name = "tourId")String tourId, Pageable pageable){
+        return ResponseEntity.ok(tourAssignmentService.getAllTourAssignments(pageable));
+    }
 }
