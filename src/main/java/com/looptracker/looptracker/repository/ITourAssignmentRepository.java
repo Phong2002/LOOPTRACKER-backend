2b6e5bbbc559eb1ee8103b5954c877586f4b518c
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 16:59:41 +0700

    [update]: existsByPassenger_Id

--- src/main/java/com/looptracker/looptracker/repository/ITourAssignmentRepository.java
+++ src/main/java/com/looptracker/looptracker/repository/ITourAssignmentRepository.java
@@ -1,7 +1,11 @@
 package com.looptracker.looptracker.repository;
 
 import com.looptracker.looptracker.entity.TourAssignment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITourAssignmentRepository extends JpaRepository<TourAssignment, String> {
+    boolean existsByPassenger_Id(Long id);
+    Page<TourAssignment> findAllByTourInstances_Id(Long tourInstanceId, Pageable pageable);
 }
