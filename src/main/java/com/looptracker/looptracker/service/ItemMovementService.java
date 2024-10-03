ebbfb877367d064780b4ce0f8d5a186e24b68c0a
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 16:56:39 +0700

    [init]: ItemMovementService

---
+++ src/main/java/com/looptracker/looptracker/service/ItemMovementService.java
@@ -0,0 +1,15 @@
+package com.looptracker.looptracker.service;
+
+import com.looptracker.looptracker.dto.request.ItemMovementRequest;
+
+public class ItemMovementService implements IItemMovementService {
+    @Override
+    public void addItem(ItemMovementRequest itemMovementRequest) {
+
+    }
+
+    @Override
+    public void removeItem(ItemMovementRequest itemMovementRequest) {
+
+    }
+}
