fcda39ded8338660ad1f378e2a2ace0082437d1f
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 17:00:09 +0700

    [update]: add addTourAssignment,updateTourAssignment,deleteTourAssignment,getAllTourAssignments,getAllTourAssignmentsByTourInstance method

--- src/main/java/com/looptracker/looptracker/service/ITourAssignmentService.java
+++ src/main/java/com/looptracker/looptracker/service/ITourAssignmentService.java
@@ -1,4 +1,14 @@
 package com.looptracker.looptracker.service;
 
+import com.looptracker.looptracker.dto.TourAssignmentDto;
+import com.looptracker.looptracker.dto.request.TourAssignmentRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ITourAssignmentService {
+    void addTourAssignment(TourAssignmentRequest tourAssignmentRequest);
+    void updateTourAssignment(TourAssignmentRequest tourAssignmentRequest);
+    void deleteTourAssignment(String id);
+    Page<TourAssignmentDto> getAllTourAssignments(Pageable pageable);
+    Page<TourAssignmentDto> getAllTourAssignmentsByTourInstance(Pageable pageable,Long tourInstanceId);
 }
