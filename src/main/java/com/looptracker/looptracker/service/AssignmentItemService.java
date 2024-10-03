f6a6cff10672d68cbd79105b0c63f2439b2cad15
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 16:57:51 +0700

    [init]: AssignmentItemService

---
+++ src/main/java/com/looptracker/looptracker/service/AssignmentItemService.java
@@ -0,0 +1,20 @@
+package com.looptracker.looptracker.service;
+
+import com.looptracker.looptracker.dto.request.AssignmentItemRequest;
+
+public class AssignmentItemService implements IAssignmentItemService {
+    @Override
+    public void assignmentItemTo(AssignmentItemRequest assignmentItemRequest) {
+
+    }
+
+    @Override
+    public void updateAssignmentItem(AssignmentItemRequest assignmentItemRequest) {
+
+    }
+
+    @Override
+    public void deleteAssignmentItem(AssignmentItemRequest assignmentItemRequest) {
+
+    }
+}
