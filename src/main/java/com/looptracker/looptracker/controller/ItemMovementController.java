97c2ee60062c75be08096f82d6a0c8710bf1894f
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-03 16:57:01 +0700

    [init]: ItemMovementController

---
+++ src/main/java/com/looptracker/looptracker/controller/ItemMovementController.java
@@ -0,0 +1,9 @@
+package com.looptracker.looptracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/item-movement")
+public class ItemMovementController {
+}
